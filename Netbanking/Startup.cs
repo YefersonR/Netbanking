eb2d5a1473f4039e55626e31dc997eb0823993cb
YefersonR <Yefersonrubio27@gmail.com>
2022-07-14 20:26:45 -0400

    Add Identity Config

--- Netbanking/Startup.cs
+++ Netbanking/Startup.cs
@@ -1,15 +1,12 @@
 using Core.Application;
+using Infrastructure.Identity;
 using Infrastructure.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace Netbanking
 {
@@ -28,7 +25,8 @@ namespace Netbanking
             services.AddControllersWithViews();
             services.AddAplicationLayer(Configuration);
             services.AddPersistenceLayer(Configuration);
-            
+            services.AddIdentityLayer(Configuration);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
