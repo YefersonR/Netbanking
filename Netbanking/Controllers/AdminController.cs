3ab3f50633f02b265501d388a8dd8c6a768f51bf
AbreuHD <abreumartinezjefferson@gmail.com>
2022-07-23 14:55:54 -0400

    Admin Controller

---
+++ Netbanking/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Core.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Netbanking.Controllers
+{
+
+    
+    public class AdminController : Controller
+    {
+        private readonly ITransationService _transation;
+
+        public AdminController(ITransationService transation)
+        {
+            _transation = transation;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Products(string ID)
+        {
+
+            return View();
+        }
+    }
+}
